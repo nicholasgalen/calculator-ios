1e7efdde11b08d55c60b6b84532536ef79d14ce8
Nicholas Galen <galennicholas@proton.me>
2025-06-28 12:25:04 -0400

    Simple calculator app using UIKit

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,11 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var firstNumTxt: UITextField!
+    @IBOutlet var secondNumTxt: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    // _ = Chamada anonima. sender = Objeto que disparou a acao. Any = Recebe qualquer tipo como parametro
+    @IBAction func addBtn(_ sender: Any) {
+        guard let (n1, n2) = getNumeros() else { return }
+        result.text = String(add(num1: n1, num2: n2))
+    }
+    
+    @IBAction func minusBtn(_ sender: Any) {
+        guard let (n1, n2) = getNumeros() else { return }
+        result.text = String(subtract(num1: n1, num2: n2))
+    }
+    
+    @IBAction func multiplierBtn(_ sender: Any) {
+        guard let (n1, n2) = getNumeros() else { return }
+        result.text = String(multiply(num1: n1, num2: n2))
+    }
+    
+    @IBAction func divisorBtn(_ sender: Any) {
+        guard let (n1, n2) = getNumeros() else { return }
+        result.text = String(divide(num1: n1, num2: n2))
+    }
+    
+    func add(num1: Double, num2: Double) -> Double{
+        return num1 + num2
+    }
+    
+    func subtract(num1: Double, num2: Double) -> Double{
+        return num1 - num2
+    }
+    func multiply(num1: Double, num2: Double) -> Double{
+        return num1 * num2
+    }
+    func divide(num1: Double, num2: Double) -> Double{
+        return num1 / num2
+    }
+    
+    func getNumeros() -> (Double, Double)? {
+        // Guard serve para tratar optionals
+        guard
+            let txt1 = firstNumTxt.text,
+            let txt2 = secondNumTxt.text,
+            let num1 = Double(txt1),
+            let num2 = Double(txt2)
+        else {
+            result.text = "Error: Invalid Number"
+            return nil
+        }
+        return (num1, num2)
+    }
 
 }
 
